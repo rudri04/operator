21bf77ddc6a6d1ff83ec102bca9f42f6d153ee12
rudri04 <therudripatel@gmail.com>
2024-03-15 11:02:40 +0530

    done

---
+++ exerccise_1.dart
@@ -0,0 +1,25 @@
+void main(){
+  int a=56,b=80;
+  print("A:$a");
+  print("B:$b");
+  print('');
+  print("Addition:");
+  print(a+b);
+  print('');
+  print("SUBTRACTION:");
+  print(a-b);
+  print('');
+  print("MULTIPLICATION:");
+  print(a*b);
+  print('');
+  print("DIVIDE:");
+  print(a/b);
+  print('');
+  print("DIVIION:");
+  print(a~/b);
+  print('');
+  print("MODULUS:");
+  print(b%a);
+  
+
+}
---
+++ exercise_2.dart
@@ -0,0 +1,6 @@
+void main(){
+    int a=99,b=12;
+    int c;
+  print("A:$a");
+  print("B:$b");
+}
